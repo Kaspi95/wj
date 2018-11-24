47f9cb97ebff29ed995592887683a6250b71a0f9
Unknown <kasperbence@gmail.com>
2018-11-24 23:24:11 +0100

    Login Screen

--- WhiskeyJudge/app/src/main/java/hu/bme/aut/whiskeyjudge/LoginActivity.java
+++ WhiskeyJudge/app/src/main/java/hu/bme/aut/whiskeyjudge/LoginActivity.java
@@ -1,7 +1,11 @@
 package hu.bme.aut.whiskeyjudge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,10 +17,36 @@ public class LoginActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-
-        setTheme(R.style.AppTheme);                                 //set back to main theme
+        //Theme(R.style.AppTheme);                                 //set back to main theme, but need only after the login page
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText etEmailAddress = findViewById(R.id.etEmailAddress);          //references for login page
+        final EditText etPassword = findViewById(R.id.etPassword);
+        final Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {                    //login button
+            @Override
+            public void onClick(final View view) {
+                if (etEmailAddress.getText().toString().isEmpty()) {
+                    etEmailAddress.requestFocus();
+                    etEmailAddress.setError("Please enter your email address");
+                    return;
+                }
+
+                if (etPassword.getText().toString().isEmpty()) {
+                    etPassword.requestFocus();
+                    etPassword.setError("Please enter your password");
+                    return;
+                }
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+
     }
 }
